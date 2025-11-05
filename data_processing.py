22da86d9c870298f31f5322c5686338641844050
Dootless <pothanathas05@gmail.com>
2025-11-05 10:50:52 +0700

    This is the modified one for answering all the questions

--- data_processing.py
+++ data_processing.py
@@ -28,36 +28,42 @@ print(sum(temps)/len(temps))
 print()
 
 # Print all cities in Germany
+print("All Germans")
+germans = []
+for cit in cities:
+    if cit['country'] == 'Germany':
+        germans.append(float(cit['temperature']))
+        print(cit)
 
-'''
-Your code here
-
-'''
 
 # Print all cities in Spain with a temperature above 12°C
+print("\nPrint all cities in Spain with a temperature above 12°C")
+for cit in cities:
+    if cit['country'] == 'Spain' and float(cit['temperature']) > 12:
+        print(cit)
 
-'''
-Your code here
-
-'''
 
 # Count the number of unique countries
+print("\nCount the number of unique countries")
+uni_count = []
+stack = 0
+for cit in cities:
+    if cit['country'] not in uni_count:
+        stack += 1
+        uni_count.append(cit['country'])
+print(stack)
 
-'''
-Your code here
-
-'''
 
 # Print the average temperature for all the cities in Germany
+print("\nPrint the average temperature for all the cities in Germany")
+total = sum(germans)
+print(f"Average: {total/len(germans)}")
 
-'''
-Your code here
-
-'''
 
 # Print the max temperature for all the cities in Italy
-
-'''
-Your code here
-
-'''
+print("\nPrint the max temperature for all the cities in Italy")
+ita = []
+for cit in cities:
+    if cit['country'] == 'Italy':
+        ita.append(float(cit['temperature']))
+print(f"Max temp in Italia: {max(ita)}")
